f689f30690fbc74974641c0ad3d2ea51c6ef581c
SiraphobCh <spcc.321@gmail.com>
2024-05-24 11:42:31 +0700

    first commit front

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AppRouter from './routes/AppRouter';
+
+function App() {
+  return (
+    <div className="min-h-screen max-w-5xl mx-auto shadow-lg">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
